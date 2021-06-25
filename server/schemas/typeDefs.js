2a9e6931e5f72e45bad53a32751cf91afd6376ae
Zack Hersh <zackshersh@gmail.com>
2021-06-25 14:50:26 -0400

    model typeDefs filled out

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,21 @@
+const { gqp } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    
+    type User {
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [bookSchema]
+    }
+
+
+`
